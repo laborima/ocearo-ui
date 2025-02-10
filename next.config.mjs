0687650a304b4d431d314d4712bcabd9f283bc3a
Matthieu Laborie <matthieu.laborie@becpg.fr>
2025-02-10 09:28:35 +0100

    Set up GitHub Pages branch

--- next.config.mjs
+++
@@ -1,4 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: process.env.NODE_ENV !== 'development', output: 'export', assetPrefix: process.env.NODE_ENV === 'production' ? '/ocearo-ui' : '' };
-
-export default nextConfig;
